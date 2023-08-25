c5dcb4781348740b3768049d9caf78a4956d0828
edwin penuela <edwinalexander20@gmail.com>
2023-08-24 21:09:09 -0400

    parameterized queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -1,6 +1,5 @@
 const { Pool } = require('pg');
 
-
 const pool = new Pool({
   user: 'labber',
   password: '123',
@@ -8,15 +7,17 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+const cohortName = process.argv[2]; // Get cohort name from command line argument
+const limit = process.argv[3]; // Get limit from command line argument
+
 pool.query(`
 SELECT students.id, students.name, students.cohort_id, cohorts.name AS cohort_name
 FROM students
 JOIN cohorts ON students.cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2]}'
-LIMIT '${process.argv[3]}';
-`)
+WHERE cohorts.name = $1
+LIMIT $2;
+`, [cohortName, limit]) // Pass cohortName and limit as parameters
 .then(res => {
-  console.log('##1 this is res ', res)
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_id} cohort`);
   })
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,6 +1,5 @@
 const { Pool } = require('pg');
 
-
 const pool = new Pool({
   user: 'labber',
   password: '123',
@@ -8,21 +7,26 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+const cohortName = process.argv[2]; // Get cohort name from command line argument
+
 pool.query(`
 SELECT teachers.name AS teacher_name, cohorts.name
 FROM teachers
 JOIN assistance_requests ON assistance_requests.teacher_id = teachers.id
 JOIN students ON students.id = assistance_requests.student_id
 JOIN cohorts ON cohorts.id = students.cohort_id
-WHERE cohorts.name = '${process.argv[2]}'
+WHERE cohorts.name = $1
 GROUP BY teachers.name, cohorts.name
 ORDER BY teachers.name ASC;
-`)
+`, [cohortName]) // Pass cohortName as a parameter
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name}: ${user.teacher_name}`);
   })
 })
 .catch(err => {
-  console.log('## 2 this is error ', err)
+  console.log('Error:', err);
+})
+.finally(() => {
+  pool.end();
 });
